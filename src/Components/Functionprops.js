8bd8c6b0f4e5692f4c2810b72e196618a6d3d370
VanessaKasei <kaseivanessa@gmail.com>
2023-09-29 21:23:40 +0300

    functionprops

---
+++ src/Components/Functionprops.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Functionprops(props) {
+    return(
+        <div>
+            <h1>Hello {props.name} from {props.place}. Welcome to Simplilearn!</h1>
+        </div>
+    );
+}
+
+export default Functionprops;
